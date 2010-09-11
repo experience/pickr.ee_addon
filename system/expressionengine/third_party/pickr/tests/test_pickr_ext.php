54ff0a41bba8bc1923b5b34955647f9301fd1a90
Stephen Lewis <git@experienceinternet.co.uk>
2010-09-12 00:41:09 +0100

    Initial commit.

---
+++ system/expressionengine/third_party/pickr/tests/test_pickr_ext.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Tests for the Pickr extension.
+ *
+ * @package		Pickr
+ * @author 		Stephen Lewis <stephen@experienceinternet.co.uk>
+ * @copyright 	Experience Internet
+ */
+
+
+/* End of file 		: test_pickr_ext.php */
+/* File location	: third_party/pickr/tests/test_pickr_ext.php */
---
+++ system/expressionengine/third_party/pickr/tests/test_pickr_model.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Tests for the Pickr model.
+ *
+ * @package			Pickr
+ * @author 			Stephen Lewis <stephen@experienceinternet.co.uk>
+ * @copyright 		Experience Internet
+ */
+
+require_once PATH_THIRD .'pickr/models/pickr_model' .EXT;
+
+class Test_pickr_model extends Testee_unit_test_case {
+	
+	
+	
+}
+
+/* End of file 		: test_pickr_model.php */
+/* File location	: third_party/pickr/tests/test_pickr_model.php */
