6f426918a3f9e97599e8513bd14891641ec9567a
Stephen Lewis <git@experienceinternet.co.uk>
2010-09-12 02:11:56 +0100

    Implement test for non-existent member.

--- system/expressionengine/third_party/pickr/tests/test_pickr_model.php
+++ system/expressionengine/third_party/pickr/tests/test_pickr_model.php
@@ -39,7 +39,7 @@ class Test_pickr_model extends Testee_unit_test_case {
 	 * TEST METHODS
 	 * ------------------------------------------------------------ */
 	
-	public function test_get_member_flickr_username()
+	public function test_get_member_flickr_username_a()
 	{
 		// Shortcuts.
 		$db 	=& $this->_ee->db;
@@ -69,6 +69,30 @@ class Test_pickr_model extends Testee_unit_test_case {
 		
 		// Run the tests.
 		$this->assertIdentical($model->get_member_flickr_username($member_id), $flickr_username);
+	}
+	
+	
+	public function test_get_member_flickr_username_b()
+	{
+		// Shortcuts.
+		$db 	=& $this->_ee->db;
+		$model	= $this->_ee->pickr_model;
+		
+		// Dummy values.
+		$flickr_username 	= 'wibble';
+		$member_id 			= '5';
+		$member_field_id	= 'm_field_id_10';
+		
+		// Query result.
+		$db_result = $this->_get_mock('query');
+		$db_result->setReturnValue('num_rows', 0);
+		
+		// Database.
+		$db->setReturnReference('select', $db);
+		$db->setReturnReference('get_where', $db_result);
+		
+		// Run the tests.
+		$this->assertIdentical($model->get_member_flickr_username($member_id), '');
 	}
 	
 }
