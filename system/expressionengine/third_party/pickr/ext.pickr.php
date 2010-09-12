6ab4aedfab2bdc421a962b1e9419c9fbe7fe9848
Stephen Lewis <git@experienceinternet.co.uk>
2010-09-12 23:51:10 +0100

    Implement shell extension.

---
+++ system/expressionengine/third_party/pickr/ext.pickr.php
@@ -0,0 +1,160 @@
+<?php if ( ! defined('BASEPATH')) exit('Direct script access is not permitted.');
+
+/**
+ * Retrieves the Flickr 'buddy icon' for a newly-registered member.
+ *
+ * @package			Pickr
+ * @author 			Stephen Lewis <stephen@experienceinternet.co.uk>
+ * @copyright 		Experience Internet
+ */
+
+class Pickr_ext {
+	
+	/* --------------------------------------------------------------
+	 * PRIVATE PROPERTIES
+	 * ------------------------------------------------------------ */
+	
+	/**
+	 * ExpressionEngine object reference.
+	 *
+	 * @access	private
+	 * @var		object
+	 */
+	private $_ee;
+	
+	
+	/* --------------------------------------------------------------
+	 * PUBLIC PROPERTIES
+	 * ------------------------------------------------------------ */
+	
+	/**
+	 * Description.
+	 *
+	 * @access	public
+	 * @var		string
+	 */
+	public $description;
+	
+	/**
+	 * Documentation URL.
+	 *
+	 * @access	public
+	 * @var		string
+	 */
+	public $docs_url;
+	
+	/**
+	 * Extension name.
+	 *
+	 * @access	public
+	 * @var		string
+	 */
+	public $name;
+	
+	/**
+	 * Does this extension have a settings screen?
+	 *
+	 * @access	public
+	 * @var		string
+	 */
+	public $settings_exist = 'n';
+	
+	/**
+	 * Version.
+	 *
+	 * @access	public
+	 * @var		string
+	 */
+	public $version;
+	
+	
+	
+	/* --------------------------------------------------------------
+	 * PUBLIC METHODS
+	 * ------------------------------------------------------------ */
+
+	/**
+	 * Class constructor.
+	 *
+	 * @access	public
+	 * @param	array 		$settings		Previously-saved extension settings.
+	 * @return	void
+	 */
+	public function __construct($settings = array())
+	{
+		$this->_ee =& get_instance();
+		
+		// Load the model.
+		$this->_ee->load->add_package_path(PATH_THIRD .'pickr/');
+		$this->_ee->load->model('pickr_model');
+		
+		// Load the language file.
+		$this->_ee->lang->loadfile('pickr');
+		
+		// Set the instance properties.
+		$this->description	= $this->_ee->lang->line('extension_description');
+		$this->docs_url		= 'http://experienceinternet.co.uk/software/pickr/';
+		$this->name			= $this->_ee->lang->line('extension_name');
+		$this->version		= $this->_ee->pickr_model->get_package_version();
+	}
+	
+	
+	/**
+	 * Activates the extension.
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	public function activate_extension()
+	{
+		$this->_ee->pickr_model->activate_extension();
+	}
+	
+	
+	/**
+	 * Disables the extension.
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	public function disable_extension()
+	{
+		$this->_ee->pickr_model->disable_extension();
+	}
+	
+	
+	/**
+	 * Updates the extension.
+	 *
+	 * @access	public
+	 * @param	string		$current_version	The current version.
+	 * @return	bool|void
+	 */
+	public function update_extension($current_version = '')
+	{
+		return $this->_ee->pickr_model->update_extension($current_version, $this->_ee->pickr_model->get_package_version());
+	}
+	
+	
+	
+	/* --------------------------------------------------------------
+	 * HOOK HANDLERS
+	 * ------------------------------------------------------------ */
+
+	/**
+	 * Handles the `member_register_validate_members` hook.
+	 *
+	 * @see		http://expressionengine.com/developers/extension_hooks/member_register_validate_members/
+	 * @access	public
+	 * @param 	string 		$member_id		The ID of the newly validated member.
+	 * @return	void
+	 */
+	public function on_member_register_validate_members($member_id)
+	{
+		
+	}
+	
+}
+
+/* End of file		: ext.pickr.php */
+/* File location	: third_party/pickr/ext.pickr.php */
---
+++ system/expressionengine/third_party/pickr/language/english/lang.pickr.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Pickr language strings.
+ *
+ * @package			Pickr
+ * @author 			Stephen Lewis <stephen@experienceinternet.co.uk>
+ * @copyright 		Experience Internet
+ */
+
+$lang = array(
+
+/* --------------------------------------------------------------
+ * REQUIRED
+ * ------------------------------------------------------------ */
+'extension_description'	=> 'Retrieves the Flickr "buddy icon" for a newly-registered member.',
+'extension_name'		=> 'Pickr',
+
+// All done.
+'' => ''
+
+);
+
+/* End of file		: lang.pickr.php */
+/* File location	: third_party/pickr/language/english/lang.pickr.php */
