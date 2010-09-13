24493fb8fa1c1eed2fdcb1bc68410ae20b303863
Stephen Lewis <git@experienceinternet.co.uk>
2010-09-13 01:51:13 +0100

    Create mock Pickr_model.

---
+++ system/expressionengine/third_party/pickr/tests/mocks/mock_pickr_model.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Pickr_model mock.
+ *
+ * @package			Pickr
+ * @author 			Stephen Lewis <stephen@experienceinternet.co.uk>
+ * @copyright 		Experience Internet
+ * @version 		0.1.0
+ */
+
+class Mock_pickr_model {
+	
+	/* --------------------------------------------------------------
+	 * PUBLIC METHODS
+	 * ------------------------------------------------------------ */
+	public function activate_extension() {}
+	public function disable_extension() {}
+	public function get_extension_class() {}
+	public function get_flickr_buddy_icon_member_field_id() {}
+	public function get_flickr_nsid_from_username() {}
+	public function get_flickr_user_buddy_icon() {}
+	public function get_flickr_username_member_field_id() {}
+	public function get_member_flickr_username() {}
+	public function get_package_name() {}
+	public function get_package_version() {}
+	public function get_site_id() {}
+	public function save_member_flickr_buddy_icon() {}
+	public function set_api_connector() {}
+	public function update_extension() {}
+	
+}
+
+/* End of file 		: mock_pickr_model.php */
+/* File location	: third_party/pickr/tests/mocks/mock_pickr_model.php */
